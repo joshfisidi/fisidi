9e4abad8a32bfa37fcfb020eece3d436368f0965
fisidi-inc <76191290+joshjgomes@users.noreply.github.com>
2023-11-15 03:43:11 -0500

    refactored ts to original structure

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -29,16 +29,16 @@ export const Navigation: React.FC = () => {
 				<div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
 					<div className="flex justify-between gap-8">
 						<Link
-							href="/projects"
+							href="/apps"
 							className="duration-200 text-zinc-400 hover:text-zinc-100"
 						>
-							App
+							Apps
 						</Link>
 						<Link
-							href="/contact"
+							href="/about"
 							className="duration-200 text-zinc-400 hover:text-zinc-100"
 						>
-							Contact
+							About
 						</Link>
 					</div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
-  { name: "Projects", href: "/projects" },
-  { name: "Contact", href: "/contact" },
+  { name: "Projects", href: "/apps" },
+  { name: "Contact", href: "/about" },
 ];
 
 export default function Home() {
